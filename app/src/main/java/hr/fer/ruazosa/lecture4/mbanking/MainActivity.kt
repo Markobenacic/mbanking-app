7d260bebad3b2976976034b56d3302e4f8d1f596
Marko Benacic <benacicmarko@gmail.com>
2020-12-01 14:45:14 +0100

    setup

---
+++ app/src/main/java/hr/fer/ruazosa/lecture4/mbanking/MainActivity.kt
@@ -0,0 +1,11 @@
+package hr.fer.ruazosa.lecture4.mbanking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
